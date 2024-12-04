e0e73b52bfd96584dcf42f7e042afca7c90bc069
churin <wl00796865@gmail.com>
2024-12-04 16:31:27 +0800

    refactor: OrderList

--- order-controller/src/components/OrderList.jsx
+++ order-controller/src/components/OrderList.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 
 const OrderList = ({ orders }) => {
+  const PendingOrders = () => (
+    <ul>
+      {orders
+        .filter(
+          (order) => order.status === "PENDING" || order.status === "DOING"
+        )
+        .map((order) => (
+          <li key={order.id}>
+            {order.type} Order #{order.id}
+          </li>
+        ))}
+    </ul>
+  );
+
+  const CompletedOrders = () => (
+    <ul>
+      {orders
+        .filter((order) => order.status === "COMPLETE")
+        .map((order) => (
+          <li key={order.id}>
+            {order.type} Order #{order.id}
+          </li>
+        ))}
+    </ul>
+  );
   return (
     <div>
       <h2>Pending Orders</h2>
-      <ul>
-        {orders
-          .filter(
-            (order) => order.status === "PENDING" || order.status === "DOING"
-          )
-          .map((order) => (
-            <li key={order.id}>
-              {order.type} Order #{order.id}
-            </li>
-          ))}
-      </ul>
+      <PendingOrders />
       <h2>Completed Orders</h2>
-      <ul>
-        {orders
-          .filter((order) => order.status === "COMPLETE")
-          .map((order) => (
-            <li key={order.id}>
-              {order.type} Order #{order.id}
-            </li>
-          ))}
-      </ul>
+      <CompletedOrders />
     </div>
   );
 };
